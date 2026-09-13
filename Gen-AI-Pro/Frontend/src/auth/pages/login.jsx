c77c3be962d01586936fd81d8edb606f07148115
Vedant Salunkhe <vedantsalunkhe54@gmail.com>
2026-09-13 13:25:42 +0000

    Register

--- Gen-AI-Pro/Frontend/src/auth/pages/login.jsx
+++ Gen-AI-Pro/Frontend/src/auth/pages/login.jsx
@@ -1,10 +1,42 @@
 import react from 'react'
+import '../auth.form.scss'
 
 const login = () => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle login logic here
+  };
+
+
   return (
-    <div className='login'>
-      Login
-    </div>
+    <main>
+      <div className="form-container">
+      <h1>Login</h1>
+
+      <form onSubmit={handleSubmit}>
+        
+        <div className="input-group">
+
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="email" placeholder='Enter your email' />
+
+        </div>
+
+        <div className="input-group">
+
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="password" placeholder='Enter your password' />
+
+        </div>
+
+        <button className='button primary-button' type='submit'>Login</button>
+        
+        
+        
+         </form>
+      </div>
+    </main>
   )
 }
 
--- Gen-AI-Pro/Frontend/src/auth/pages/register.jsx
+++ Gen-AI-Pro/Frontend/src/auth/pages/register.jsx
@@ -1,8 +1,45 @@
 const Register = () => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle register logic here
+  };
+
   return (
-    <div className="register">
-      Register
-    </div>
+     <main>
+      <div className="form-container">
+      <h1>Register</h1>
+
+      <form onSubmit={handleSubmit}>
+        
+        <div className="input-group">
+
+          <label htmlFor="username">Username</label>
+          <input type="text" name="username" id="username" placeholder='Enter your username' />
+
+        </div>
+
+        <div className="input-group">
+
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="email" placeholder='Enter your email' />
+
+        </div>
+
+        <div className="input-group">
+
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="password" placeholder='Enter your password' />
+
+        </div>
+
+        <button className='button primary-button' type='submit'>Register</button>
+        
+        
+        
+         </form>
+      </div>
+    </main>
   );
 };
 
