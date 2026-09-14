94f416f8d92b0245a3636ad248732a4dba2422e4
Vedant Salunkhe <vedantsalunkhe54@gmail.com>
2026-09-14 11:43:47 +0000

    Gen_AI

--- Gen-AI-Pro/Backend/Routes/auth.routes.js
+++ Gen-AI-Pro/Backend/Routes/auth.routes.js
@@ -1,16 +1,20 @@
-const { Router } = require('express');
-const authController = require('../controllers/auth.controller');
-const authMiddleware = require('../middleware/auth.middlewawre');
+const { Router } = require("express");
 
+const authController = require("../controllers/auth.controller");
+const authMiddleware = require("../middleware/auth.middleware");
 
 const authRouter = Router();
 
-authRouter.post('/register', authController.registerUser);
-authRouter.post('/login', authController.loginUser);
+authRouter.post("/register", authController.registerUser);
 
+authRouter.post("/login", authController.loginUser);
 
-authRouter.get('/logout', authController.logoutUser);
-authRouter.get('/get-me', authMiddleware.authenticateToken, authController.getMeController);
+authRouter.get("/logout", authController.logoutUser);
 
+authRouter.get(
+    "/get-me",
+    authMiddleware.authenticateToken,
+    authController.getMeController
+);
 
 module.exports = authRouter;
