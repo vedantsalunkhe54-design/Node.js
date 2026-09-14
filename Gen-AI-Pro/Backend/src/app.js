537c49a4eb82404457ec18e41cca6741d94869da
Vedant Salunkhe <vedantsalunkhe54@gmail.com>
2026-09-14 11:48:54 +0000

    auth

--- Gen-AI-Pro/Backend/src/app.js
+++ Gen-AI-Pro/Backend/src/app.js
@@ -1,12 +1,17 @@
 const express = require ('express');
 const authRouter = require('../Routes/auth.routes');
 const cookieParser = require('cookie-parser');
+const cors = require ("cors");
 
 const app = express();
 
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}))
 
 app.use('/api/auth', authRouter);
 
