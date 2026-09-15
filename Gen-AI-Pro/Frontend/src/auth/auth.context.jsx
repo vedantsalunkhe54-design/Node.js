b3160ea51fd6f88b0d1d777510a9431101671629
Vedant Salunkhe <vedantsalunkhe54@gmail.com>
2026-09-15 16:27:16 +0000

    Auth

--- Gen-AI-Pro/Frontend/src/auth/auth.context.jsx
+++ Gen-AI-Pro/Frontend/src/auth/auth.context.jsx
@@ -1,10 +1,23 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+import { getMe } from "./services/auth.api";
 
 export const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+
+        const getAndSetUser = async() => {
+            const data = await getMe()
+            setUser(data.user)
+            setLoading(false)
+        }
+
+        getAndSetUser
+
+    })
 
     return (
         <AuthContext.Provider
